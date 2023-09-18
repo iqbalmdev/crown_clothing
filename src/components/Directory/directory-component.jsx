1798d4f5d07a3390382b7c16317ea6c462b143ae
Iqbal <mohamediqbalm@xbi4.com>
2023-09-18 09:36:17 +0530

    Updated directory,category,scss files in the project

---
+++ src/components/Directory/directory-component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import CategoryItem from "../category-item/catgeory-item.component";
+import "./directory.component.styles.scss";
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+const DirectoryComp = () => {
+  return (
+    <div className="directories-container">
+      {categories?.map((category) => (
+        <CategoryItem category={category} key={category?.id} />
+      ))}
+    </div>
+  );
+};
+
+export default DirectoryComp;
